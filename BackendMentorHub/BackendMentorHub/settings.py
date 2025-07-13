2dcc293cba787f8b343fc444df186c30c8e8afed
nikeshjs <nikeshjs123@gmail.com>
2025-07-13 20:04:43 +0545

    setup

--- BackendMentorHub/BackendMentorHub/settings.py
+++ BackendMentorHub/BackendMentorHub/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BackendMentorHub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
